ae3234e0fe0428f245dea090cfe2e1a02f96b433
odugbesanyemi <odugbesanyemi@gmail.com>
2022-05-03 12:24:33 +0100

    changes 5/3/22

---
+++ gallerio/js/main.js
@@ -0,0 +1,57 @@
+//this is to get the pictures from the folder and change the src attribure of the frame img
+//1st step is to target the buttons and the img frame
+let natureBtn = document.querySelector('.natureBtn');
+let techBtn = document.querySelector('.techBtn');
+let loveBtn = document.querySelector('.loveBtn');
+let target = document.querySelector('.frame img');
+let prevBtn = document.querySelector('#prev');
+let nextBtn = document.querySelector('#next');
+let picNum;
+
+natureBtn.onclick = () => {
+    picNum = 1;
+    //let the frame img src choose from
+    target.setAttribute('src',"assets/nature/"+ picNum  + ".jpg");
+    prevBtn.onclick = () => {
+        if(picNum < 5 && picNum != 1 ){
+            picNum--;
+            target.setAttribute('src',"assets/nature/"+ picNum  + ".jpg");
+        }
+        if(picNum == 1){
+            //do nothing
+        }
+    }
+    nextBtn.onclick = () => {
+        if(picNum < 5){
+            picNum ++;
+            target.setAttribute('src',"assets/nature/"+ picNum  + ".jpg");
+        }
+        else if(picNum = 0){
+            picNum = 1
+        }
+    }
+}
+
+techBtn.onclick = () => {
+    picNum = 1;
+    //let the frame img src choose from
+    target.setAttribute('src',"assets/tech/"+ picNum  + ".jpg");
+    prevBtn.onclick = () => {
+        if(picNum <= 5 && picNum != 1){
+            picNum--;
+            target.setAttribute('src',"assets/tech/"+ picNum  + ".jpg");
+        }
+        if(picNum == 1){
+            //do nothing
+        }
+    }
+    nextBtn.onclick = () => {
+        if(picNum < 5){
+            picNum ++;
+            target.setAttribute('src',"assets/tech/"+ picNum  + ".jpg");
+        }
+        else if(picNum = 0){
+            picNum = 1
+        }
+    }
+}
